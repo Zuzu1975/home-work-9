174b3948a431ba22ceb0fbe5c8e0127da19c040e
Zuzu work <muratov01@mail.ru>
2023-05-07 18:55:31 +0600

    Домашка 9

---
+++ main.js
@@ -0,0 +1,85 @@
+                                // Home work 9
+
+
+
+
+       // 1 задание
+
+// let btn = document.querySelector(".cheсker");
+// let out = document.querySelector('.out');
+
+//       let a =[1,2,3,4,5,6,7,8]
+    
+// btn.onclick = function () {   
+//     let out = '';  
+//       console.log(a[1]);
+//       console.log(a[4]);
+//       console.log(a[2]);
+//       console.log(a[7]);   
+//       console.log(a[0]); 
+//       out += i +''
+     
+// }
+// document.querySelector('.out').innerHTML= out
+
+
+       // 2 задание
+
+// let btn = document.querySelector(".cheсker");
+// let out = document.querySelector('.out');
+//       let a =[1,2,3,4,5,6,7,8,9,10]
+// btn.onclick = function () {   
+//       console.log(a[1]);
+//       console.log(a[3]);
+//       console.log(a[5]);
+//       console.log(a[7]);   
+//       console.log(a[9]); 
+      
+// }
+
+        // 3 задание
+
+
+
+// let btn = document.querySelector(".cheсker");
+// let out = document.querySelector('.out');
+
+//       let a =[1,2,3,4,5,6,7,8,9,10]
+    
+// btn.onclick = function () {   
+  
+//       console.log(a[0]);
+//       console.log(a[2]);
+//       console.log(a[4]);
+//       console.log(a[6]);   
+//       console.log(a[8]); 
+   
+// }
+
+
+
+         // 4 задание
+
+let btn = document.querySelector(".cheсker");
+let out = document.querySelector('.out');
+
+      let b = [
+          [1,2,3],    
+          [4,5,6],   
+          [7,8,9]
+          
+      ]
+   
+btn.onclick = function () {   
+
+      console.log(b[2][2]);
+      console.log(b[2][1]);
+      console.log(b[2][0]);
+      console.log(b[1][2]);   
+      console.log(b[1][1]); 
+      console.log(b[1][0]);
+      console.log(b[0][2]);
+      console.log(b[0][1]);   
+      console.log(b[0][0]); 
+     
+}
